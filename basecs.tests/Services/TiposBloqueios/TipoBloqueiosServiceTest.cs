a967db595e2584156d109a83974dfb1d9ac8880d
Alessandro dos Santos <professor_alessandro@hotmail.com>
2021-09-25 10:53:05 -0300

    #Commit before implements swagger

---
+++ basecs.tests/Services/TiposBloqueios/TipoBloqueiosServiceTest.cs
@@ -0,0 +1,75 @@
+﻿using basecs.Interfaces.ITiposBloqueiosService;
+using basecs.Services;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace basecs.tests.Services.TipoBloqueio
+{
+    public class TipoBloqueiosServiceTest
+    {
+        #region ATRIBUTTES
+        TiposBloqueiosService _service;
+        Mock<ITiposBloqueiosService> _serviceMock;
+        #endregion
+
+        #region CONSTRUCTORS
+        public TipoBloqueiosServiceTest()
+        {
+            _serviceMock = new Mock<ITiposBloqueiosService>();
+            _service = new TiposBloqueiosService(new Data.MyDbContext());
+        }
+        #endregion
+
+        #region INSERT
+        [Fact(DisplayName = "Insert Send Valid Post")]
+        public void Insert_SendValidPost()
+        {
+            var retorno = new basecs.Models.TipoBloqueio { TipoBloqueioId = 1, Descricao = "Unit Test", Ativo = true, UsuarioUltimaAlteracaoId = 1, UsuarioInclusaoId = 1, DataInclusao = DateTime.Now, DataUltimaAlteracao = DateTime.Now };
+            _serviceMock.Setup(x => x.Insert(It.IsAny<basecs.Models.TipoBloqueio>())).ReturnsAsync(retorno);
+        }
+
+        [Fact(DisplayName = "Insert Send Invalid PostId")]
+        public async Task Insert_SendInvalidPostId()
+        {
+            await Assert.ThrowsAsync<Exception>(() => _service.Insert(new basecs.Models.TipoBloqueio { TipoBloqueioId = 200, Descricao = "Unit Test", Ativo = true, UsuarioUltimaAlteracaoId = 1, UsuarioInclusaoId = 1, DataInclusao = DateTime.Now, DataUltimaAlteracao = DateTime.Now }));
+        }
+
+        [Fact(DisplayName = "Insert Send Invalid Post Description")]
+        public async Task Insert_SendInvalidPostDescription()
+        {
+            await Assert.ThrowsAsync<Exception>(() => _service.Insert(new basecs.Models.TipoBloqueio { TipoBloqueioId = 0, Descricao = "D", Ativo = true, UsuarioUltimaAlteracaoId = 1, UsuarioInclusaoId = 1, DataInclusao = DateTime.Now, DataUltimaAlteracao = DateTime.Now }));
+        }
+        #endregion
+
+        #region UPDATE
+        [Fact(DisplayName = "Update Send Invalid Post Description")]
+        public void Update_SendValidPut()
+        {
+            var retorno = new basecs.Models.TipoBloqueio { TipoBloqueioId = 200, Descricao = "Unit Test", Ativo = true, UsuarioUltimaAlteracaoId = 1, UsuarioInclusaoId = 1, DataInclusao = DateTime.Now, DataUltimaAlteracao = DateTime.Now };
+            _serviceMock.Setup(x => x.Update(It.IsAny<basecs.Models.TipoBloqueio>())).ReturnsAsync(retorno);
+        }
+
+        [Fact]
+        public async Task Update_SendInvalidPostId()
+        {
+            await Assert.ThrowsAsync<Exception>(() => _service.Update(new basecs.Models.TipoBloqueio { TipoBloqueioId = 0, Descricao = "Unit Test", Ativo = true, UsuarioUltimaAlteracaoId = 1, UsuarioInclusaoId = 1, DataInclusao = DateTime.Now, DataUltimaAlteracao = DateTime.Now }));
+        }
+
+        [Fact]
+        public async Task Update_SendInvalidPostDescription()
+        {
+            await Assert.ThrowsAsync<Exception>(() => _service.Update(new basecs.Models.TipoBloqueio { TipoBloqueioId = 6, Descricao = "D", Ativo = true, UsuarioUltimaAlteracaoId = 1, UsuarioInclusaoId = 1, DataInclusao = DateTime.Now, DataUltimaAlteracao = DateTime.Now }));
+        }
+        #endregion
+
+        #region DELETE
+        [Fact(DisplayName = "Delete Send Valid Post")]
+        public void Delete_SendValidDelete()
+        {
+            _serviceMock.Setup(x => x.Delete(It.IsAny<int>()));
+        }
+        #endregion
+    }
+}
---
+++ basecs/Services/TiposBloqueiosService.cs
@@ -0,0 +1,180 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using basecs.Data;
+using basecs.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using basecs.Business.TiposBloqueios;
+using basecs.Helpers.Helpers.Validators;
+using basecs.Interfaces.ITiposBloqueiosService;
+
+namespace basecs.Services
+{
+    public class TiposBloqueiosService : ITiposBloqueiosService
+    {
+        #region ATRIBUTTES
+        private readonly MyDbContext _context;
+        private readonly TiposBloqueiosBusiness _business;
+        #endregion
+
+        #region CONTRUCTORS
+        public TiposBloqueiosService(MyDbContext context)
+        {
+            _context = context;
+            _business = new TiposBloqueiosBusiness();
+        }
+        #endregion
+
+        #region FIND BY ID
+        public async Task<TipoBloqueio> FindById(int id)
+        {
+            try
+            {
+                return await this._context.TiposBloqueios.SingleOrDefaultAsync(c => c.TipoBloqueioId == id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Houve um erro ao buscar o TipoBloqueio desejada!" + ex.Message);
+            }
+        }
+        #endregion
+
+        #region RETURN LIST WITH PARAMETERS PAGINATED
+        public async Task<List<TipoBloqueio>> ReturnListWithParametersPaginated(
+                int? id,
+                string descricao,
+                bool? ativo,
+                int? pageNumber,
+                int? rowspPage
+            )
+        {
+            try
+            {
+                SqlParameter[] Params = {
+                    new SqlParameter("@Id", id.Equals(null) ? DBNull.Value : id),
+                    new SqlParameter("@Descricao", string.IsNullOrEmpty(Validators.RemoveInjections(descricao)) ? DBNull.Value : Validators.RemoveInjections(descricao)),
+                    new SqlParameter("@Ativo", ativo.Equals(null) ? DBNull.Value : ativo),
+                    new SqlParameter("@PageNumber", pageNumber),
+                    new SqlParameter("@RowspPage", rowspPage)
+                };
+
+                var storedProcedure = $@"[dbo].[TiposBloqueiosPaginated] @Id, @Descricao, @Ativo, @PageNumber, @RowspPage";
+
+                using (var context = this._context)
+                {
+                    return await context.TiposBloqueios.FromSqlRaw(storedProcedure, Params).ToListAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Não foi possível realizar a busca por tipos bloqueios: " + ex.Message);
+            }
+
+        }
+        #endregion
+
+        #region RETURN LIST WITH PARAMETERS
+        public async Task<List<TipoBloqueio>> ReturnListWithParameters(
+                int? id,
+                string descricao,
+                bool? ativo
+            )
+        {
+            try
+            {
+                using (var context = this._context)
+                {
+                    return await context.TiposBloqueios.Where(c =>
+                    (c.TipoBloqueioId == id || id == null) &&
+                    (c.Descricao.Contains(Validators.RemoveInjections(descricao)) || string.IsNullOrEmpty(Validators.RemoveInjections(descricao))) &&
+                    (c.Ativo == ativo || ativo == null)
+                    ).OrderByDescending(x => x.TipoBloqueioId)
+                    .ToListAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Não foi possível realizar a busca por tipos bloqueios: " + ex.Message);
+            }
+        }
+        #endregion
+
+        #region INSERT
+        public async Task<TipoBloqueio> Insert(TipoBloqueio model)
+        {
+            try
+            {
+                string validationMessage = _business.InsertValidation(model);
+
+                if (validationMessage.Equals(""))
+                {
+                    this._context.TiposBloqueios.Add(model);
+                    await this._context.SaveChangesAsync();
+                    return model;
+                }
+                else
+                {
+                    throw new Exception(validationMessage);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Houve um erro ao incluir tipos bloqueios: " + ex.Message);
+            }
+        }
+        #endregion
+
+        #region UPDATE
+        public async Task<TipoBloqueio> Update(TipoBloqueio model)
+        {
+            try
+            {
+                string validationMessage = _business.UpdateValidation(model);
+
+                if (validationMessage.Equals(""))
+                {
+                    this._context.TiposBloqueios.Update(model);
+                    await this._context.SaveChangesAsync();
+                    return model;
+                }
+                else
+                {
+                    throw new Exception(validationMessage);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Houve um erro ao tentar editar o registro: " + ex.Message);
+            }
+        }
+        #endregion        
+
+        #region DELETE SERVIÇO DE DELETE COMENTADO
+        public async Task<TipoBloqueio> Delete(int id)
+        {
+            try
+            {
+                string validationMessage = _business.DeleteValidation(id);
+
+                if (validationMessage.Equals(""))
+                {
+                    TipoBloqueio model = await this.FindById(id);
+                    model.Ativo = false;
+                    await this.Update(model);
+                    return model;
+                }
+                else
+                {
+                    throw new Exception(validationMessage);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Houve um erro ao tentar deletar o registro: " + ex.Message);
+            }
+        }
+        #endregion
+    }
+}
