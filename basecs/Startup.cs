9617db32434546ed397de8f21a9490697cbb490f
Alessandro dos Santos <professor_alessandro@hotmail.com>
2021-09-30 11:02:18 -0300

    #Test

--- basecs/Startup.cs
+++ basecs/Startup.cs
@@ -8,8 +8,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
