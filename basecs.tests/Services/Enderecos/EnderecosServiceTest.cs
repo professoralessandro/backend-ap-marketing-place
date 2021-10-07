d28ab2fd8eb55f0a28f737dc6deea641d6cfd072
Alessandro dos Santos <professor_alessandro@hotmail.com>
2021-10-07 14:49:31 -0300

    test

--- basecs.tests/Services/Enderecos/EnderecosServiceTest.cs
+++ basecs.tests/Services/Enderecos/EnderecosServiceTest.cs
@@ -27,20 +27,20 @@ namespace basecs.tests.Services.Enderecos
         [Fact(DisplayName = "Insert Send Valid Post")]
         public void Insert_SendValidPost()
         {
-            var retorno = new basecs.Models.Endereco { EnderecoId = 1, Logradouro = "Unit Test", Complemento = "CASA", Bairro = "Unit Test", Cidade = "Sao Paulo", Cep = "123456", Estado = "SP", TipoEnderecoId = 1, Numero = "10", IsPrincipal = true, Ativo = true, UsuarioUltimaAlteracaoId = 1, UsuarioInclusaoId = 1, DataInclusao = DateTime.Now, DataUltimaAlteracao = DateTime.Now };
+            var retorno = new basecs.Models.Endereco { EnderecoId = 1, Logradouro = "Unit Test", PontoReferencia = "", Complemento = "CASA", Bairro = "Unit Test", Cidade = "Sao Paulo", Cep = "123456", Estado = "SP", TipoEnderecoId = 1, Numero = "10", IsPrincipal = true, Ativo = true, UsuarioUltimaAlteracaoId = 1, UsuarioInclusaoId = 1, DataInclusao = DateTime.Now, DataUltimaAlteracao = DateTime.Now };
             _serviceMock.Setup(x => x.Insert(It.IsAny<basecs.Models.Endereco>())).ReturnsAsync(retorno);
         }
 
         [Fact(DisplayName = "Insert Send Invalid PostId")]
         public async Task Insert_SendInvalidPostId()
         {
-            await Assert.ThrowsAsync<Exception>(() => _service.Insert(new basecs.Models.Endereco { EnderecoId = 200, Logradouro = "Unit Test", Complemento = "CASA", Bairro = "Unit Test", Cidade = "Sao Paulo", Cep = "123456", Estado = "SP", TipoEnderecoId = 1, Numero = "10", IsPrincipal = true, Ativo = true, UsuarioUltimaAlteracaoId = 1, UsuarioInclusaoId = 1, DataInclusao = DateTime.Now, DataUltimaAlteracao = DateTime.Now }));
+            await Assert.ThrowsAsync<Exception>(() => _service.Insert(new basecs.Models.Endereco { EnderecoId = 200, Logradouro = "Unit Test", PontoReferencia = "", Complemento = "CASA", Bairro = "Unit Test", Cidade = "Sao Paulo", Cep = "123456", Estado = "SP", TipoEnderecoId = 1, Numero = "10", IsPrincipal = true, Ativo = true, UsuarioUltimaAlteracaoId = 1, UsuarioInclusaoId = 1, DataInclusao = DateTime.Now, DataUltimaAlteracao = DateTime.Now }));
         }
 
         [Fact(DisplayName = "Insert Send Invalid Post Description")]
         public async Task Insert_SendInvalidPostDescription()
         {
-            await Assert.ThrowsAsync<Exception>(() => _service.Insert(new basecs.Models.Endereco { EnderecoId = 0, Logradouro = "D", Complemento = "CASA", Bairro = "Unit Test", Cidade = "Sao Paulo", Cep = "123456", Estado = "SP", TipoEnderecoId = 1, Numero = "10", IsPrincipal = true, Ativo = true, UsuarioUltimaAlteracaoId = 1, UsuarioInclusaoId = 1, DataInclusao = DateTime.Now, DataUltimaAlteracao = DateTime.Now }));
+            await Assert.ThrowsAsync<Exception>(() => _service.Insert(new basecs.Models.Endereco { EnderecoId = 0, Logradouro = "D", PontoReferencia = "", Complemento = "CASA", Bairro = "Unit Test", Cidade = "Sao Paulo", Cep = "123456", Estado = "SP", TipoEnderecoId = 1, Numero = "10", IsPrincipal = true, Ativo = true, UsuarioUltimaAlteracaoId = 1, UsuarioInclusaoId = 1, DataInclusao = DateTime.Now, DataUltimaAlteracao = DateTime.Now }));
         }
         #endregion
 
@@ -48,20 +48,20 @@ namespace basecs.tests.Services.Enderecos
         [Fact(DisplayName = "Update Send Invalid Post Description")]
         public void Update_SendValidPut()
         {
-            var retorno = new basecs.Models.Endereco { EnderecoId = 200, Logradouro = "Unit Test", Complemento = "CASA", Bairro = "Unit Test", Cidade = "Sao Paulo", Cep = "123456", Estado = "SP", TipoEnderecoId = 1, Numero = "10", IsPrincipal = true, Ativo = true, UsuarioUltimaAlteracaoId = 1, UsuarioInclusaoId = 1, DataInclusao = DateTime.Now, DataUltimaAlteracao = DateTime.Now };
+            var retorno = new basecs.Models.Endereco { EnderecoId = 200, Logradouro = "Unit Test", Complemento = "CASA", PontoReferencia = "", Bairro = "Unit Test", Cidade = "Sao Paulo", Cep = "123456", Estado = "SP", TipoEnderecoId = 1, Numero = "10", IsPrincipal = true, Ativo = true, UsuarioUltimaAlteracaoId = 1, UsuarioInclusaoId = 1, DataInclusao = DateTime.Now, DataUltimaAlteracao = DateTime.Now };
             _serviceMock.Setup(x => x.Update(It.IsAny<basecs.Models.Endereco>())).ReturnsAsync(retorno);
         }
 
         [Fact]
         public async Task Update_SendInvalidPostId()
         {
-            await Assert.ThrowsAsync<Exception>(() => _service.Update(new basecs.Models.Endereco { EnderecoId = 0, Logradouro = "Unit Test", Complemento = "CASA", Bairro = "Unit Test", Cidade = "Sao Paulo", Cep = "123456", Estado = "SP", TipoEnderecoId = 1, Numero = "10", IsPrincipal = true, Ativo = true, UsuarioUltimaAlteracaoId = 1, UsuarioInclusaoId = 1, DataInclusao = DateTime.Now, DataUltimaAlteracao = DateTime.Now }));
+            await Assert.ThrowsAsync<Exception>(() => _service.Update(new basecs.Models.Endereco { EnderecoId = 0, Logradouro = "Unit Test", PontoReferencia = "", Complemento = "CASA", Bairro = "Unit Test", Cidade = "Sao Paulo", Cep = "123456", Estado = "SP", TipoEnderecoId = 1, Numero = "10", IsPrincipal = true, Ativo = true, UsuarioUltimaAlteracaoId = 1, UsuarioInclusaoId = 1, DataInclusao = DateTime.Now, DataUltimaAlteracao = DateTime.Now }));
         }
 
         [Fact]
         public async Task Update_SendInvalidPostDescription()
         {
-            await Assert.ThrowsAsync<Exception>(() => _service.Update(new basecs.Models.Endereco { EnderecoId = 6, Logradouro = "D", Complemento = "CASA", Bairro = "Unit Test", Cidade = "Sao Paulo", Cep = "123456", Estado = "SP", TipoEnderecoId = 1, Numero = "10", IsPrincipal = true, Ativo = true, UsuarioUltimaAlteracaoId = 1, UsuarioInclusaoId = 1, DataInclusao = DateTime.Now, DataUltimaAlteracao = DateTime.Now }));
+            await Assert.ThrowsAsync<Exception>(() => _service.Update(new basecs.Models.Endereco { EnderecoId = 6, Logradouro = "D", PontoReferencia = "", Complemento = "CASA", Bairro = "Unit Test", Cidade = "Sao Paulo", Cep = "123456", Estado = "SP", TipoEnderecoId = 1, Numero = "10", IsPrincipal = true, Ativo = true, UsuarioUltimaAlteracaoId = 1, UsuarioInclusaoId = 1, DataInclusao = DateTime.Now, DataUltimaAlteracao = DateTime.Now }));
         }
         #endregion
 
